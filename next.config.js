4acb6fbf88e05161c63d414d4918e329aba4ea59
Joaquin <joakonice@gmail.com>
2025-04-10 21:52:45 -0300

    fix: update dependencies and next config for deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  images: {
+    domains: ['api.maptiler.com'],
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig 
