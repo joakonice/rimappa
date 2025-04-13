a6c264e2839e9e29dfac32713001d5e98607af88
Joaquin <joakonice@gmail.com>
2025-04-12 22:33:25 -0300

    fix: add User type to NextAuth

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,6 +1,13 @@
 import 'next-auth';
 
 declare module 'next-auth' {
+  interface User {
+    id: string;
+    email: string;
+    name: string;
+    role: 'USER' | 'ADMIN' | 'ORGANIZER' | 'COMPETITOR';
+  }
+
   interface Session {
     user: {
       id: string;
