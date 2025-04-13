dd037e63f9850cef4a4448480d3c4ed9434cfa0a
Joaquin <joakonice@gmail.com>
2025-04-13 00:14:14 -0300

    feat: switch to CARTO Positron map style

--- src/app/dashboard/competitions/page.tsx
+++ src/app/dashboard/competitions/page.tsx
@@ -409,7 +409,7 @@ export default function CompetitionsPage() {
             </div>}>
               <div className="h-full w-full">
                 <Map
-                  mapStyle="https://demotiles.maplibre.org/style.json"
+                  mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
                   initialViewState={{
                     longitude: viewport.longitude,
                     latitude: viewport.latitude,
